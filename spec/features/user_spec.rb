441ee5cda44258f5321b05df6027a96bb7c5030c
Jenny Allar <JennyAllar@gmail.com>
2014-06-10 11:02:37 -0600

    Got rspec working correctly.

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 
 feature 'User Functions' do
-  scenario 'user should see a welcome message' do
+  scenario 'user should see a sign in/up options' do
     visit '/'
-    expect(page).to have_content 'Welcome to The Picture Closet!'
+    expect(page).to have_content 'Sign up | Login'
   end
 end
